814c63769a190b47733e83091ac9db8a86aa8b4d
ChrisNassif <chrisjnassif@gmail.com>
2024-12-26 16:10:35 +0000

    got motor controller and closed loop control code to work

--- src/microros/src/common_microros_libs.h
+++ src/microros/src/common_microros_libs.h
@@ -6,8 +6,9 @@
 #include <rclc/executor.h>
 #include <std_msgs/msg/int32.h>
 #include <std_msgs/msg/float32.h>
-#include <rcl_interfaces/msg/log.h>
+#include <std_msgs/msg/string.h>
 #include <rmw_microros/rmw_microros.h>
+#include <rcl_interfaces/msg/log.h>
 
 // Pico SDK libraries
 #include <pico/stdlib.h>
@@ -15,10 +16,30 @@
 
 // Important constants
 #define LED_PIN 25
+#define TIMEOUT_MS 1000
+#define AGENT_ATTEMPTS 120
 
 // Pin declarations
+// I2C
 #define SDA_PIN 4
 #define SCL_PIN 5
 
+// SPI
+#define SCLK_PIN 10
+#define MISO_PIN 12
+#define MOSI_PIN 11
+
+// Chip Selections
+#define CS_RM_PIN 13
+#define CS_WM_PIN 15
+#define CS_RE_PIN 22
+#define CS_WE_PIN 26
+
+// Miscellaneous
+#define SLP_RM_PIN 19
+#define SLP_WM_PIN 14
+#define MOTOCTL 27
+
 // Port declarations
 #define I2C_PORT i2c0
+#define SPI_PORT spi1
---
+++ src/microros/src/rudder_control.c
@@ -0,0 +1,140 @@
+#include "rudder_control.h"
+#include "polulu_HAL.c"
+#include "magnetometer_HAL.c"
+#include "encoder_read.c"
+#include <math.h>
+
+#define ACCEPTABLE_RUDDER_ERROR 1
+#define MOTOR_ANGLE_OFFSET 36
+#define RUDDER_GAIN 300
+#define MAX_RUDDER_SPEED 50
+#define MAX_RUDDER_CURRENT 2000
+
+#define CLOCKWISE 1
+#define COUNTER_CLOCKWISE 0
+
+#define MAX_RUDDER_ANGLE 20
+#define MIN_RUDDER_ANGLE -20
+
+const float MID_RUDDER_ANGLE = (MAX_RUDDER_ANGLE + MIN_RUDDER_ANGLE) / 2;
+const int MAX_RUDDER_ERROR = (MAX_RUDDER_ANGLE - MIN_RUDDER_ANGLE);
+
+#define STEP_PERIOD_US 2000
+
+rcl_node_t rudder_control_node;
+rcl_timer_t sensor_transmission_timer;
+
+rcl_subscription_t desired_rudder_angle_subscriber;
+rcl_publisher_t current_rudder_angle_publisher;
+rcl_publisher_t magnetometer_angle_publisher;
+
+std_msgs__msg__Float32 magnetometer_angle_msg;
+std_msgs__msg__Float32 current_rudder_angle_msg;
+std_msgs__msg__Float32 desired_rudder_angle_msg;
+
+static drv8711 rudderDriver;
+static amt22 rudderEncoder;
+static cmps14 compass;
+
+static float desiredRudderAngleSynch = 0;
+
+#define RCCHECK(fn) {                   \
+    rcl_ret_t temp_rc = fn;             \
+    if ((temp_rc == RCL_RET_ERROR))     \
+    {                                   \
+        gpio_put(LED_PIN, 1);           \
+    }                                   \
+}
+
+
+void rudder_control_init(rcl_allocator_t *allocator, rclc_support_t *support, rclc_executor_t *executor) {
+    rclc_node_init_default(&rudder_control_node, "rudder_control", "", support);
+
+    rclc_subscription_init_default(
+        &desired_rudder_angle_subscriber,
+        &rudder_control_node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+        "/actions/rudder_angle"
+    );
+
+    rclc_publisher_init_default(
+        &magnetometer_angle_publisher,
+        &rudder_control_node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+        "/heading"
+    );
+
+    rclc_timer_init_default(
+        &sensor_transmission_timer,
+        support,
+        RCL_MS_TO_NS(5),
+        rudder_control_callback
+    );
+
+    rclc_executor_add_timer(executor, &sensor_transmission_timer);
+
+    spi_init(SPI_PORT, 500 * 1000);
+    gpio_set_function(SCLK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(MOSI_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(MISO_PIN, GPIO_FUNC_SPI);
+
+    AMT22_init(&rudderEncoder, 22, SPI_PORT);
+
+    drv8711_init(&rudderDriver, CS_RM_PIN, SLP_RM_PIN, SPI_PORT);
+    drv8711_clearStatus(&rudderDriver);
+    drv8711_setDecayMode(&rudderDriver, AutoMixed);
+    drv8711_setCurrent(&rudderDriver, 2000);
+    drv8711_setStepMode(&rudderDriver, MicroStep4);
+    drv8711_enableDriver(&rudderDriver);
+    drv8711_setAwake(&rudderDriver);
+
+    gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(SDA_PIN);
+    gpio_pull_up(SCL_PIN);
+
+    cmps14_init(&compass, I2C_PORT, 0x60);
+
+    current_rudder_angle_msg.data = 0.0;
+    magnetometer_angle_msg.data = 0.0;
+
+    RCCHECK(rclc_executor_add_subscription(executor, &desired_rudder_angle_subscriber, &current_rudder_angle_msg, &desired_rudder_angle_received_callback, ON_NEW_DATA));
+
+}
+
+
+void desired_rudder_angle_received_callback(const void *msg_in) {
+    const std_msgs__msg__Float32 *desired_rudder_angle_msg = (const std_msgs__msg__Float32 *)msg_in;
+    desiredRudderAngleSynch = desired_rudder_angle_msg->data;
+}
+ 
+
+void rudder_control_callback() {
+    float current_rudder_angle = get_motor_angle(&rudderEncoder) - MOTOR_ANGLE_OFFSET;
+
+    if (current_rudder_angle >= 180) {
+        current_rudder_angle -= 360;
+    }
+
+    float rudder_error = current_rudder_angle - desiredRudderAngleSynch;
+
+    if (abs(rudder_error) > ACCEPTABLE_RUDDER_ERROR) {
+        if (((int)rudder_error % 360) > 0 && ((int)rudder_error % 360) < 180) 
+            drv8711_setDirection(&rudderDriver, COUNTER_CLOCKWISE);
+    
+        else 
+            drv8711_setDirection(&rudderDriver, CLOCKWISE);
+
+        int number_of_steps = (int)(abs(rudder_error) * MAX_RUDDER_SPEED / MAX_RUDDER_ERROR);   
+        for (int i = 0; i < number_of_steps; i++) {
+            drv8711_step(&rudderDriver);
+            sleep_us(2000);
+        }
+    }
+
+    magnetometer_angle_msg.data = cmps14_getBearing(&compass);
+
+    rcl_publish(&magnetometer_angle_publisher, &magnetometer_angle_msg, NULL);
+    // rcl_publish(&magnetometer_angle_publisher, &magnetometer_angle_msg, NULL);
+
+}
