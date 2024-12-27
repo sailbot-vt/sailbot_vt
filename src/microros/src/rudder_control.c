e35f37a820fe40113bfee0cfe9ff4f07c13a055d
Chris Nassif <chrisjnassif@gmail.com>
2024-12-27 15:35:23 -0500

    fully functioning motor controller

--- src/microros/src/rudder_control.c
+++ src/microros/src/rudder_control.c
@@ -6,15 +6,15 @@
 
 #define ACCEPTABLE_RUDDER_ERROR 1
 #define MOTOR_ANGLE_OFFSET 36
-#define RUDDER_GAIN 200
-#define MAX_RUDDER_SPEED 50
+#define RUDDER_GAIN 300
 #define MAX_RUDDER_CURRENT 2000
+#define MIN_TIME_BETWEEN_MOTOR_STEPS 2000 // microseconds
 
 #define CLOCKWISE 1
 #define COUNTER_CLOCKWISE 0
 
-#define MAX_RUDDER_ANGLE 20
-#define MIN_RUDDER_ANGLE -20
+#define MAX_RUDDER_ANGLE 35
+#define MIN_RUDDER_ANGLE -35
 
 const float MID_RUDDER_ANGLE = (MAX_RUDDER_ANGLE + MIN_RUDDER_ANGLE) / 2;
 const int MAX_RUDDER_ERROR = (MAX_RUDDER_ANGLE - MIN_RUDDER_ANGLE);
@@ -22,17 +22,20 @@ const int MAX_RUDDER_ERROR = (MAX_RUDDER_ANGLE - MIN_RUDDER_ANGLE);
 #define STEP_PERIOD_US 2000
 
 rcl_node_t  rudder_control_node;
-rcl_timer_t rudder_control_loop_timer;
+rmw_qos_profile_t best_effort_qos_profile;
 
+rcl_timer_t rudder_control_loop_timer;
 rcl_service_t      zero_rudder_encoder_service;
 rcl_subscription_t should_relay_be_open_subscriber;
 rcl_subscription_t desired_rudder_angle_subscriber;
 rcl_publisher_t    current_rudder_angle_publisher;
+rcl_publisher_t    current_rudder_motor_angle_publisher;
 rcl_publisher_t    magnetometer_angle_publisher;
 
 
 std_msgs__msg__Bool           should_relay_be_open_msg;  
 std_msgs__msg__Float32        magnetometer_angle_msg;
+std_msgs__msg__Float32        current_rudder_motor_angle_msg;
 std_msgs__msg__Float32        current_rudder_angle_msg;
 std_msgs__msg__Float32        desired_rudder_angle_msg;
 std_srvs__srv__Empty_Request  empty_request_msg;
@@ -44,10 +47,12 @@ static cmps14  compass;
 
 
 static float desired_rudder_angle = 0;
+static float desired_rudder_motor_angle  = 0;
 
 
 void rudder_control_init(rcl_allocator_t *allocator, rclc_support_t *support, rclc_executor_t *executor) {
     RCCHECK(rclc_node_init_default(&rudder_control_node, "rudder_control", "", support));
+
 
     RCCHECK(rclc_service_init_default(
         &zero_rudder_encoder_service, 
@@ -63,7 +68,7 @@ void rudder_control_init(rcl_allocator_t *allocator, rclc_support_t *support, rc
         "/should_relay_be_open"
     ));
     
-    RCCHECK(rclc_subscription_init_default(
+    RCCHECK(rclc_subscription_init_best_effort(
         &desired_rudder_angle_subscriber,
         &rudder_control_node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
@@ -75,6 +80,13 @@ void rudder_control_init(rcl_allocator_t *allocator, rclc_support_t *support, rc
         &rudder_control_node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
         "/current_rudder_angle"
+    ));
+
+    RCCHECK(rclc_publisher_init_default(
+        &current_rudder_motor_angle_publisher,
+        &rudder_control_node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
+        "/current_rudder_motor_angle"
     ));
 
     RCCHECK(rclc_publisher_init_default(
@@ -157,6 +169,7 @@ void rudder_control_init(rcl_allocator_t *allocator, rclc_support_t *support, rc
     // Initialize Relay
     gpio_init(RELAY_PIN);
     gpio_set_dir(RELAY_PIN, GPIO_OUT);
+    gpio_put(RELAY_PIN, 0);
 
     desired_rudder_angle_msg.data = 0.0;
     current_rudder_angle_msg.data = 0.0;
@@ -165,17 +178,25 @@ void rudder_control_init(rcl_allocator_t *allocator, rclc_support_t *support, rc
 
 
 inline float get_rudder_angle_from_motor_angle(float motor_angle) {
-    return -2.094e-5 * pow(motor_angle, 3) + 0.001259 * pow(motor_angle, 2) + 0.4159 * pow(motor_angle, 1) - 8.373 - MOTOR_ANGLE_OFFSET;
+    return -0.00002094 * pow(motor_angle, 3) + 0.001259 * pow(motor_angle, 2) + 0.4159 * motor_angle - 8.373;
 }
 
 inline float get_motor_angle_from_rudder_angle(float rudder_angle) {
-    return 0.001345 * pow(rudder_angle, 3) + 0.003741 * pow(rudder_angle, 2) + 2.142 * rudder_angle + 19.71 + MOTOR_ANGLE_OFFSET;
+    return 0.001345 * pow(rudder_angle, 3) + 0.003741 * pow(rudder_angle, 2) + 2.142 * rudder_angle + 19.71;
 }
 
 
 void desired_rudder_angle_received_callback(const void *msg_in) {
     const std_msgs__msg__Float32 *desired_rudder_angle_msg = (const std_msgs__msg__Float32 *)msg_in;
     desired_rudder_angle = desired_rudder_angle_msg->data;
+
+    if (desired_rudder_angle > MAX_RUDDER_ANGLE) 
+        desired_rudder_angle = MAX_RUDDER_ANGLE;
+
+    if (desired_rudder_angle < MIN_RUDDER_ANGLE)
+        desired_rudder_angle = MIN_RUDDER_ANGLE;
+
+    desired_rudder_motor_angle = get_motor_angle_from_rudder_angle(desired_rudder_angle);
 }
 
 
@@ -193,14 +214,15 @@ void zero_rudder_encoder_callback(const void * request_msg, void * response_msg)
 
 
 void rudder_control_loop() {
-    float current_rudder_motor_angle = get_motor_angle(&rudderEncoder);
-    float current_rudder_angle = get_rudder_angle_from_motor_angle(current_rudder_motor_angle);
+    float current_rudder_motor_angle = get_motor_angle(&rudderEncoder) + MOTOR_ANGLE_OFFSET;     // motor_angle % 360
 
-    if (current_rudder_angle >= 180) {
-        current_rudder_angle -= 360;
+    if (current_rudder_motor_angle >= 180) {
+        current_rudder_motor_angle -= 360;
     }
 
-    float rudder_error = current_rudder_angle - desired_rudder_angle;
+    float current_rudder_angle = get_rudder_angle_from_motor_angle(current_rudder_motor_angle);
+
+    float rudder_error = current_rudder_motor_angle - desired_rudder_motor_angle;
 
     if (abs(rudder_error) > ACCEPTABLE_RUDDER_ERROR) {
         if (((int)rudder_error % 360) > 0 && ((int)rudder_error % 360) < 180) 
@@ -209,16 +231,23 @@ void rudder_control_loop() {
         else 
             drv8711_setDirection(&rudderDriver, CLOCKWISE);
 
-        int number_of_steps = (int)(abs(rudder_error) * MAX_RUDDER_SPEED / MAX_RUDDER_ERROR);   
+        int number_of_steps = (int)(abs(rudder_error) * RUDDER_GAIN / MAX_RUDDER_ERROR);   
+
+        if (number_of_steps > 50) {
+            number_of_steps = 50;
+        }
+
         for (int i = 0; i < number_of_steps; i++) {
             drv8711_step(&rudderDriver);
-            sleep_us(2000);
+            sleep_us(MIN_TIME_BETWEEN_MOTOR_STEPS);
         }
     }
 
     magnetometer_angle_msg.data = cmps14_getBearing(&compass) / 10.0;
     current_rudder_angle_msg.data = current_rudder_angle;
+    current_rudder_motor_angle_msg.data = current_rudder_motor_angle;
 
+    rcl_publish(&current_rudder_motor_angle_publisher, &current_rudder_motor_angle_msg, NULL);
     rcl_publish(&current_rudder_angle_publisher, &current_rudder_angle_msg, NULL);
     rcl_publish(&magnetometer_angle_publisher, &magnetometer_angle_msg, NULL);
 
