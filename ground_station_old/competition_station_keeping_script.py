842da58bf15e9c47ec90bc10abdb13d51bfd9446
Chris Nassif <chrisjnassif@gmail.com>
2025-06-10 15:58:01 +0000

    accounted for magnetic deviation

--- ground_station_old/competition_station_keeping_script.py
+++ ground_station_old/competition_station_keeping_script.py
@@ -1,7 +1,12 @@
-import json, requests
+import json, requests, time
 TELEMETRY_SERVER_URL = "http://18.191.164.84:8080/"
 
-
+start_time = time.time()
 while True:
-    waypoints_list = json.load(open("desired_waypoints.json"))["waypoints"]
-    print(requests.post(TELEMETRY_SERVER_URL + "waypoints/set", json={"value": waypoints_list}).json())
+    print(time.time()- start_time)
+    if (time.time() - start_time) > 300:
+        waypoints_list = json.load(open("desired_waypoints2.json"))["waypoints"]
+        print(requests.post(TELEMETRY_SERVER_URL + "waypoints/set", json={"value": waypoints_list}).json())
+        time.sleep(1)
+    
+    time.sleep(0.05)
