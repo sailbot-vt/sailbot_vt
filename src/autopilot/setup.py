7f53e22c39bf22d0d668e8402ccb12b72580022e
Chris Nassif <chrisjnassif@gmail.com>
2025-06-28 14:49:50 +0000

    simplified autopilot setup.py

--- src/autopilot/setup.py
+++ src/autopilot/setup.py
@@ -14,9 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        
-        ('lib/' + package_name, glob('autopilot/*.py')),
-        ('lib/' + package_name, glob('autopilot_library/*.py')),
+
         ('lib/' + package_name, glob('config/*'))
     ],
     install_requires=['setuptools'],
